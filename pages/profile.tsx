22879db8b85605c92990632bda2e238c5ecced0f
Yvanoski Sanon <72044858+Yvponiz@users.noreply.github.com>
2023-03-14 13:56:16 -0400

    added profile page + updated readme

---
+++ pages/profile.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import Layout from "../components/layout";
+
+const Profile: NextPage = () =>{
+    return (
+        <Layout>
+            <span style={{
+                display:'flex',
+                justifyContent:'center',
+                alignItems:'center',
+                minHeight:'100vh',
+                fontSize:'2rem'
+            }}>Coming Soon</span>
+        </Layout>
+    )
+}
+
+export default Profile
