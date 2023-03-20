11dbd9712ac297c3259d5eb02948e77ce9d5ffe8
Yvan <yvanoski_s@hotmail.com>
2023-03-19 22:29:19 -0400

    added props to profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,15 +1,20 @@
-import { NextPage } from "next";
+import { NextApiRequest, NextApiResponse, NextPage } from "next";
 import Layout from "../components/layout";
+import commonProps, { UserProps } from "../utils/commonProps";
 
-const Profile: NextPage = () =>{
+export function getServerSideProps({ req, res }: { req: NextApiRequest, res: NextApiResponse }) {
+    return commonProps({ req, res })
+}
+
+const Profile: NextPage<UserProps> = ({isLoggedIn, id, username, email}) => {
     return (
-        <Layout>
+        <Layout isLoggedIn={isLoggedIn}>
             <span style={{
-                display:'flex',
-                justifyContent:'center',
-                alignItems:'center',
-                minHeight:'100vh',
-                fontSize:'2rem'
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh',
+                fontSize: '2rem'
             }}>Coming Soon</span>
         </Layout>
     )
